558496ba3bbafaf48f1c6268bc970dbf95c96363
kyw <rla4062002@naver.com>
2021-11-25 23:18:17 +0900

    Create account Controller Create SecurityConfig

---
+++ src/main/java/com/studylecture/account/AccountController.java
@@ -0,0 +1,16 @@
+package com.studylecture.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model){
+        return "account/sign-up";
+    }
+
+}
---
+++ src/main/java/com/studylecture/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.studylecture.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+                        "/email-login", "/check-email-login", "/login-link").permitAll()
+                // post, get 다 허용
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                // get 만 허용
+                .anyRequest().authenticated();
+                // 나머지는 로그인 해야만 가능
+    } // configure
+
+
+}
