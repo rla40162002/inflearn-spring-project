89c7737e99b3c785a4619135cc37c0026d2cbd16
kyw <rla4062002@naver.com>
2021-11-25 21:50:01 +0900

    First commit

---
+++ src/test/java/com/studylecture/studylecture/StudylectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studylecture.studylecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudylectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
