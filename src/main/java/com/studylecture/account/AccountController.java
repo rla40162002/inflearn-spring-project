42bf5a1a7da55917e5fbc7b06241466c3707485a
kyw <rla4062002@naver.com>
2021-11-28 21:42:44 +0900

    Validate sign up form

--- src/main/java/com/studylecture/account/AccountController.java
+++ src/main/java/com/studylecture/account/AccountController.java
@@ -1,17 +1,48 @@
 package com.studylecture.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+    private final SignUpFormValidator signUpFormValidator;
 
+
+    @InitBinder("signUpForm") // 33번째줄 변수명이 아닌 클래스명을 따라간다.
+
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
-    public String signUpForm(Model model){
+    public String signUpForm(Model model) {
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) { // @ModelAttribute 생략가능
+        if (errors.hasErrors()) { // 백엔드 검사하는 부분
+            return "account/sign-up";
+        }
+
+    /*       signUpFormValidator.validate(signUpForm, errors);
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }*/
+
+        // TODO: 회원가입 처리
+        return "redirect:/";
+
+    } // signUpSubmit
 }
---
+++ src/main/java/com/studylecture/account/SignUpFormValidator.java
@@ -0,0 +1,33 @@
+package com.studylecture.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor // private final 타입의 멤버 variable 을 생성자를 만들어준다.
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class); // SignUpForm 타입의 인스턴스를 검사
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        // TODO email, nickname db에서 조회해서 중복 여부 검사
+        SignUpForm signUpForm = (SignUpForm) errors;
+
+        if(accountRepository.existsByEmail(signUpForm.getEmail())){
+            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일입니다.");
+        }
+
+        if(accountRepository.existsByNickname(signUpForm.getNickname())){
+            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "이미 사용중인 닉네임입니다.");
+        }
+
+    } // validate
+}
