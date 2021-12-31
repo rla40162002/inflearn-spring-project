95b7322720c4752f17bbd84b126dd8eec8125eb7
kyw <rla4062002@naver.com>
2021-12-31 19:43:44 +0900

    Add Zone domain and init data City list of Korea to csv file

---
+++ src/main/java/com/studylecture/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.studylecture.zone;
+
+import com.studylecture.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
