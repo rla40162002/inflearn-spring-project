984167a6bce7c5316db0b9a15ac6904c5d707729
kyw <rla4062002@naver.com>
2022-01-20 01:52:26 +0900

    Add notification icon Fix login error page

--- src/main/java/com/studylecture/infra/config/SecurityConfig.java
+++ src/main/java/com/studylecture/infra/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .mvcMatchers("/node_modules/**")
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .antMatchers("/favicon.ico", "/resources/**", "/error");
     }
 }
--- src/main/java/com/studylecture/modules/notification/NotificationRepository.java
+++ src/main/java/com/studylecture/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.studylecture.modules.notification;
 
+import com.studylecture.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
